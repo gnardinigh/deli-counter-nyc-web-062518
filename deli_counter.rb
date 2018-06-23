2a9c4b0edaacbb0032d8701103cdd01d8537fc4e
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-23 22:57:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+
+
+def line(katz_deli)
+  
+  if katz_deli.size==0
+    puts "The line is currently empty."
+    
+  else
+    line_statement = "The line is currently: "
+    counter=0
+    while counter < katz_deli.size-1
+      line_statement+="#{counter+1}. #{katz_deli[counter]} "
+      counter++
+    end
+    line_statement+="#{counter+1}. #{katz_deli[counter]}"
+    puts line_statement
+  end
+  
+end
+
+
+
+
+
+
+
+def take_a_number(katz_deli, new_person)
+  katz_deli.push(new_person)
+  puts "Welcome, #{new_person}. You are number #{katz_deli.size} in line."
+  katz_deli
+end
+
+def now_serving(katz_deli)
+  puts "Now serving #{katz_deli[0]}."
+  katz_deli.unshift
+  katz_deli
+end
